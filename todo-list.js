859604b6cc57efb5dc60cd39220fcb482cbfea16
Brian Fontillas <BrianKFontillas@gmail.com>
2023-05-29 20:16:04 -0700

    added filter and foreach

--- todo-list.js
+++ todo-list.js
@@ -28,3 +28,85 @@ class Todo {
     return this.title;
   }
 }
+
+class TodoList {
+  constructor(title) {
+    this.title = title;
+    this.todos = [];
+  }
+
+  add(todo) {
+    if (!(todo instanceof Todo)) {
+      throw new TypeError("can only add Todo objects");
+    }
+
+    this.todos.push(todo);
+  }
+
+  size() {
+    return this.todos.length;
+  }
+
+  first() {
+    return this.todos[0];
+  }
+
+  last() {
+    return this.todos[this.size() - 1];
+  }
+
+  itemAt(index) {
+    this._validateIndex(index);
+    return this.todos[index];
+  }
+
+  _validateIndex(index) {
+    if (!(index in this.todos)) {
+      throw new ReferenceError(`invalid index: ${index}`);
+    }
+  }
+
+  markDoneAt(index) {
+    this.itemAt(index).markDone();
+  }
+
+  markUndoneAt(index) {
+    this.itemAt(index).markUndone();
+  }
+
+  isDone() {
+    return this.todos.every(todo => todo.isDone());
+  }
+
+  shift() {
+    this.todos.shift();
+  }
+
+  pop() {
+    this.todos.pop();
+  }
+
+  removeAt(index) {
+    this._validateIndex(index);
+    return this.todos.splice(index, 1);
+  }
+
+  toString() {
+    let title = `---- ${this.title} ----`;
+    let list = this.todos.map(todo => todo.toString()).join("\n");
+    return `${title}\n${list}`;
+  }
+
+  forEach(func) {
+    this.todos.forEach(func);
+  }
+
+  filter(func) {
+    let newList = new TodoList(this.title);
+    this.forEach(todo => {
+      if (func(todo)) newList.add(todo);
+    });
+
+    return newList;
+  }
+}
